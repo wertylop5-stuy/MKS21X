dfcd914906df4b91c00ae2968583dd2b1d2a1e33
Stanley Lin <slin55055@gmail.com>
2016-10-02 10:55:18 -0400

    Finished HW08

--- HW06/Triangle/Driver.java
+++ HW06/Triangle/Driver.java
@@ -1,21 +1,40 @@
 public class Driver {
     public static void main(String args[]) {
-	Point a = new Point();
-	a.setX(1);
-	a.setY(0);
-	Point b = new Point(a);
-	b.setX(0);
+		Point a = new Point();
+		a.setX(1);
+		a.setY(0);
+		Point b = new Point(a);
+		b.setX(0);
 
-	System.out.println("a: " + a.getX() + " b: " + b.getX());
-	a.distance(b);
-	System.out.println(a.distance(b));
+		System.out.println("a: " + a.getX() + " b: " + b.getX());
+		a.distance(b);
+		System.out.println(a.distance(b));
 	
-	Triangle t = new Triangle(a, b, new Point(0, 5));
-	System.out.println("Perim: " + t.getPerimeter());
-	System.out.println("Area: " + t.getArea());
-	System.out.println("isRight: " + t.isRight());
-	Triangle t2 = new Triangle(new Point(0, 0), new Point(4, 0),
-				   new Point(2, 3));
-	System.out.println("isRight t2: " + t2.isRight());
+		Triangle t = new Triangle(a, b, new Point(0, 5));
+		System.out.println("Perim: " + t.getPerimeter());
+		System.out.println("Area: " + t.getArea());
+		System.out.println("isRight: " + t.isRight());
+		Triangle t2 = new Triangle(new Point(0, 0), new Point(4, 0),
+					   new Point(2, 3));
+		System.out.println("isRight t2: " + t2.isRight());
+		Triangle g = new Triangle(0, 0, 5, 0, 0, 12);
+		System.out.println("Perim: " + g.getPerimeter());
+		System.out.println("Area: " + g.getArea());
+		System.out.println("isRight: " + g.isRight());
+		
+		g = new Triangle(0, 0, 0, 0, 0, 0);
+		System.out.println("Perim: " + g.getPerimeter());
+		System.out.println("Area: " + g.getArea());
+		System.out.println("isRight: " + g.isRight());
+		
+		g = new Triangle(0, 0, 1, 1, 2, 2);
+		System.out.println("Perim: " + g.getPerimeter());
+		System.out.println("Area: " + g.getArea());
+		System.out.println("isRight: " + g.isRight());
+		
+		g = new Triangle(0, 0, -6, 0, 0, -8);
+		System.out.println("Perim: " + g.getPerimeter());
+		System.out.println("Area: " + g.getArea());
+		System.out.println("isRight: " + g.isRight());
     }
 }
--- HW06/Triangle/Triangle.java
+++ HW06/Triangle/Triangle.java
@@ -13,29 +13,35 @@ public class Triangle{
 	 Point object as an argument and copies its members into the
 	 new object.
 	 */
-	v1 = new Point(a);
-	v2 = new Point(b);
-	v3 = new Point(c);
+		/*v1 = new Point(a);
+		v2 = new Point(b);
+		v3 = new Point(c);*/
+		
+		//nvm, here's the normal way
+		v1 = new Point(a.getX(), a.getY());
+		v2 = new Point(b.getX(), b.getY());
+		v3 = new Point(c.getX(), c.getY());
     }
     
     public Triangle(double x1,double y1,
 		    double x2,double y2,
 		    double x3,double y3) {
-	v1 = new Point(x1, y1);
-	v2 = new Point(x2, y2);
-	v3 = new Point(x3, y3);
+		v1 = new Point(x1, y1);
+		v2 = new Point(x2, y2);
+		v3 = new Point(x3, y3);
     }
     
 
     //format: "Triangle @ (1,1) (3,2) (5,6)"
+    @Override
     public String toString(){
-	return "Triangle @ (" + v1.getX() + "," + v1.getY() + ") " +
-	    "(" + v2.getX() + "," + v2.getY() + ") " +
-	    "(" + v3.getX() + "," + v3.getY() + ")";
+		return "Triangle @ (" + v1.getX() + "," + v1.getY() + ") " +
+			"(" + v2.getX() + "," + v2.getY() + ") " +
+			"(" + v3.getX() + "," + v3.getY() + ")";
     }
 
     public double getPerimeter(){
-	return v1.distance(v2) + v2.distance(v3) + v3.distance(v1);
+		return v1.distance(v2) + v2.distance(v3) + v3.distance(v1);
     }
 
     //Uses heron's formula
@@ -47,10 +53,10 @@ public class Triangle{
 	 getting the semiperimeter is simple with the getPerimeter
 	 method.
 	 */
-	double s = getPerimeter() / 2.0;
-	return Math.sqrt(s * (s - v1.distance(v2)) *
-			 (s - v2.distance(v3)) *
-			 (s - v3.distance(v1)));
+		double s = getPerimeter() / 2.0;
+		return Math.sqrt(s * (s - v1.distance(v2)) *
+				 (s - v2.distance(v3)) *
+				 (s - v3.distance(v1)));
     }
 
     //do not use == to check if doubles are the same, because of rounding errors + irrational numbers.
@@ -59,22 +65,22 @@ public class Triangle{
     //must be within (1/1000)%
     private boolean isCloseEnough(double a,double b){
         //3. Explain how to calculate.
-	/*
-	  Get the absolute value of the diffence of the two values.
-	  Get value of largest # / 1000 (epsilon)
-	  If abs val less than epsilon, true, else false;
-	 */
-	double epsilon;
-	//System.out.println(a + " " + b);
-	if (a > b) epsilon = a / 1000;
-	else epsilon = b / 1000;
-	//System.out.println(epsilon);
-	return Math.abs(a - b) <= epsilon;
+		/*
+		  Get the absolute value of the diffence of the two values.
+		  Get value of largest # / 1000 (epsilon)
+		  If abs val less than epsilon, true, else false;
+		 */
+		double epsilon;
+		//System.out.println(a + " " + b);
+		if (a > b) epsilon = a / 1000;
+		else epsilon = b / 1000;
+		//System.out.println(epsilon);
+		return Math.abs(a - b) <= epsilon;
     }
 
     //helper function for isRight, assume that ab is the longest side
-    private boolean pyCheck(Point a, Point b, Point c){
-	System.out.println(a + " " + b + " " + c);
+    private boolean pycheck(Point a, Point b, Point c){
+	//System.out.println(a + " " + b + " " + c);
 	System.out.println(Math.sqrt( Math.pow(b.distance(c), 2) +
 				      Math.pow(c.distance(a), 2)) + " " + 
 			   a.distance(b));
@@ -87,35 +93,38 @@ public class Triangle{
     //It is suggested that you use pycheck 
     //Return true when the triangle is a right triangle, false otherwise
     public boolean isRight(){
-	double ab = v1.distance(v2);
-	double bc = v2.distance(v3);
-	double ca = v3.distance(v1);
+    	if (isCloseEnough(getArea(), 0)) return false;
+    	
+		double ab = v1.distance(v2);
+		double bc = v2.distance(v3);
+		double ca = v3.distance(v1);
 
-	if (ab >= bc) {
-	    System.out.println(1);
-	    if (ab >= ca) return pyCheck(v1, v2, v3);
-	    return pyCheck(v1, v3, v2);
-	}
-	else if (bc >= ca) {
-	    System.out.println(2);
-	    return pyCheck(v2, v3, v1);
-	}
-	System.out.println(3);
-	return pyCheck(v3, v1, v2);
+		if (ab >= bc) {
+			//System.out.println(1);
+			if (ab >= ca) return pycheck(v1, v2, v3);
+			return pycheck(v1, v3, v2);
+		}
+		else if (bc >= ca) {
+			//System.out.println(2);
+			return pycheck(v2, v3, v1);
+		}
+		//System.out.println(3);
+		return pycheck(v3, v1, v2);
     }
 
     public static void main(String args[]) {
-	Triangle t = new Triangle(new Point(0, 0),
-				  new Point(3, 0),
-				  new Point(0, 4));
-	System.out.println("isCloseEnough tests");
-	System.out.println(t.isCloseEnough(1, 2));
-	System.out.println(t.isCloseEnough(1000, 999));
-	System.out.println(t.isCloseEnough(100000, 99999.0002));
-	System.out.println(t.isCloseEnough(0, 0));
-	System.out.println("Testing pyCheck");
-	System.out.println(t);
-	System.out.println(t.pyCheck(t.v1, t.v2, t.v3));
+		Triangle t = new Triangle(new Point(0, 0),
+					  new Point(3, 0),
+					  new Point(0, 4));
+		/*System.out.println("isCloseEnough tests");
+		System.out.println(t.isCloseEnough(1, 2));
+		System.out.println(t.isCloseEnough(1000, 999));
+		System.out.println(t.isCloseEnough(100000, 99999.0002));
+		System.out.println(t.isCloseEnough(0, 0));
+		System.out.println(t.isCloseEnough(100000, 99999));*/
+		System.out.println("Testing pyCheck");
+		System.out.println(t);
+		System.out.println(t.pycheck(t.v1, t.v2, t.v3));
     }
 
 }

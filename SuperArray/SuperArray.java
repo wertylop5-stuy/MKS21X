d03075481bfb7faa15a4a498ed0b8cb2a2dd11ca
Stanley Lin <slin55055@gmail.com>
2016-10-28 10:24:40 -0400

    continueing

--- SuperArray/SuperArray.java
+++ SuperArray/SuperArray.java
@@ -1,84 +1,89 @@
 /*
-auto expand array
-add to the end/begin
-remove elems
-toString
-get/set at index
+  auto expand array
+  add to the end/begin
+  remove elems
+  toString
+  get/set at index
 */
 public class SuperArray {
     private int[] mData;
     private int mSize;
 	
-	//constructor make an empty superArray should make size 0, but the data capacity 10.
+    //constructor make an empty superArray should make size 0, but the data capacity 10.
     public SuperArray() {
-		mData = new int[10];
-		mSize = 0;
+	mData = new int[10];
+	mSize = 0;
     }
 
-	//index is the spot where the element will be inserted
-	 private void shiftRight(int index) {
-		 if (mSize == mData.length) {
-			 grow();
-		 }
-		 for (int x = mSize - 1; x >= index; x--) {
-			 mData[x+1] = mData[x];
-		 }
-	 }
+    //index is the spot where the element will be inserted
+    private void shiftRight(int index) {
+	if (mSize == mData.length) {
+	    grow();
+	}
+	for (int x = mSize - 1; x >= index; x--) {
+	    mData[x+1] = mData[x];
+	}
+    }
 	 
-	 //index is the spot where the removed element was
-	 private void shiftLeft(int index) {
-		 for (int x = index; x < mSize - 1; x++) {
-			 mData[x] = mData[x+1];
-		 }
-	 }
+    //index is the spot where the removed element was
+    private void shiftLeft(int index) {
+	for (int x = index; x < mSize - 1; x++) {
+	    mData[x] = mData[x+1];
+	}
+    }
 	
-	 //2
-     /**Resize the data, by making a new array, then copying over elements, use this as your data.*/
+    //2
+    /**Resize the data, by making a new array, then copying over elements, use this as your data.*/
     private void grow(){
-		int[] temp = new int[mData.length*2];
-		for (int x = 0; x < mData.length; x++) {
-			temp[x] = mData[x];	 
-		}
-		mData = temp;
+	int[] temp = new int[mData.length*2];
+	for (int x = 0; x < mData.length; x++) {
+	    temp[x] = mData[x];	 
+	}
+	mData = temp;
     }
 
     //1
     /**add the value n to the next available slot in the superArray.
      *this will change the size. This function should always work
      *And will resize the SuperArray if needed.*/
-     public void add(int n){
-		 if (mSize == mData.length) {
-			 grow();
-		 }
-		 mData[mSize++] = n;
-     }
+    public void add(int n){
+	if (mSize == mData.length) {
+	    grow();
+	}
+	mData[mSize++] = n;
+    }
 	 
-	 //Add a value at a certain index
-	 public void add(int n, int index) {
-		 shiftRight(index);
-		 mData[index] = n;
-		 mSize++;
-	 }
+    //Add a value at a certain index
+    public boolean add(int index, int n) {
+	shiftRight(index);
+	mData[index] = n;
+	mSize++;
+	return true;
+    }
 	 
-	 public void remove(int index) {
-		 shiftLeft(index);
-		 mSize--;
-	 }
+    public int remove(int index) {
+	int temp = mData[index];
+	shiftLeft(index);
+	mSize--;
+	return temp;
+    }
 	 
-	 public void replace(int index, int n) {
-		 mData[index] = n;
-	 }
+    public int set(int index, int n) {
+	int temp = mData[index];
+	mData[index] = n;
+	return temp;
+    }
 
     //3
     /**format the super array like this :   [ 1, 3, 6, 8, 23, 99, -4, 5] 
      *commas between... square bracket start/end and no comma at end.*/
     public String toString(){
-		String res = "[";
-		for (int x = 0; x < mSize; x++) {
-			res += mData[x];
-			if (x != mSize - 1) res += ", ";
-		}
-		return res + "]";
+	String res = "[";
+	for (int x = 0; x < mSize; x++) {
+	    res += mData[x];
+	    if (x != mSize - 1) res += ", ";
+	}
+	return res + "]";
     }
 
     //4
@@ -87,38 +92,50 @@ public class SuperArray {
      *commas between... square bracket start/end and no comma at end.
      *unused slots should be printed as _ (underscores) */
     public String toStringDebug(){
-		int x;
-		String res = "[";
-		for (x = 0; x < mSize; x++) {
-			res += mData[x];
-			if (x != mSize - 1) res += ", ";
-		}
-		if (x != mData.length) res += ", ";
-		for (; x < mData.length; x++) {
-			res+= "_";
-			if (x != mData.length - 1) res += ", ";
-		}
-		return res + "]";
-    }
-	
-	public int get(int index) {
-		 return mData[index];
+	int x;
+	String res = "[";
+	for (x = 0; x < mSize; x++) {
+	    res += mData[x];
+	    if (x != mSize - 1) res += ", ";
 	}
-	
-	public int size(){
-		return mSize;
+	if (x != mData.length) res += ", ";
+	for (; x < mData.length; x++) {
+	    res+= "_";
+	    if (x != mData.length - 1) res += ", ";
+	}
+	return res + "]";
     }
+    
+    public int[] toArray() {
+	int[] res = new int[mSize];
+	for (int x = 0; x < mSize; x++) {
+	    res[x] = mData[x];
+	}
+	return res;
+    }
+
+    public void clear() { mSize = 0; }
+    
+    public boolean isEmpty() { return mSize == 0; }
 	
+    public int get(int index) { return mData[index]; }
+	
+    public int size(){ return mSize; }
 
     public static void main(String[] args) {
-		SuperArray s = new SuperArray();
-		for (int x = 0; x < 11; x++) {
-			s.add(x);
-		}
-		//System.out.println(s.toStringDebug());
-		s.add(4, 0);
-		s.remove(1);
-		System.out.println(s.toStringDebug());
-		System.out.println(s.size());
+	SuperArray s = new SuperArray();
+	for (int x = 0; x < 11; x++) {
+	    s.add(x);
+	}
+	//System.out.println(s.toStringDebug());
+	s.add(0, 1);
+	int f = s.set(1, 1);
+	f = s.remove(3);
+	System.out.println(s.toStringDebug());
+	System.out.println(f);
+	int[] test = s.toArray();
+	int[] test1 = new SuperArray().to
+	System.out.println("testing toArray");
+	for (int x : test) System.out.println(x);
     }
 }

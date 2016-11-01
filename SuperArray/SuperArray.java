65c2ab1823cb454698ca7da1166847def90b63ab
Stanley Lin <slin55055@gmail.com>
2016-11-01 00:13:42 -0400

    Finished SuperArray

--- SuperArray/SuperArray.java
+++ SuperArray/SuperArray.java
@@ -5,6 +5,11 @@
   toString
   get/set at index
 */
+
+/*
+TODO
+need to fix toStringDebug
+*/
 public class SuperArray {
     private int[] mData;
     private int mSize;
@@ -35,7 +40,11 @@ public class SuperArray {
     //2
     /**Resize the data, by making a new array, then copying over elements, use this as your data.*/
     private void grow(){
-		int[] temp = new int[mData.length*2];
+		int[] temp;
+		if (mData.length < 1) {
+			temp = new int[10];
+		}
+		else temp = new int[mData.length*2];
 		for (int x = 0; x < mData.length; x++) {
 			temp[x] = mData[x];	 
 		}
@@ -77,6 +86,7 @@ public class SuperArray {
     //3
     /**format the super array like this :   [ 1, 3, 6, 8, 23, 99, -4, 5] 
      *commas between... square bracket start/end and no comma at end.*/
+	@Override
     public String toString(){
 		String res = "[";
 		for (int x = 0; x < mSize; x++) {
@@ -92,14 +102,17 @@ public class SuperArray {
      *commas between... square bracket start/end and no comma at end.
      *unused slots should be printed as _ (underscores) */
     public String toStringDebug(){
+		if (mData.length == 0) return "[]";
+		//if (mData.length == 1) return "[_]";
 		int x;
 		String res = "[";
 		for (x = 0; x < mSize; x++) {
 			res += mData[x];
 			if (x != mSize - 1) res += ", ";
 		}
-		if (x != mData.length) res += ", ";
-		for (; x < mData.length; x++) {
+		if (x == 0) res += "_";
+		if (x != mData.length && mData.length > 1) res += ", ";
+		for (x++; x < mData.length; x++) {
 			res+= "_";
 			if (x != mData.length - 1) res += ", ";
 		}
@@ -129,7 +142,7 @@ public class SuperArray {
 	}
     
     public void trimToSize() {
-	mData = toArray();
+		mData = toArray();
     }
     
     public void clear() { mSize = 0; }
@@ -139,30 +152,89 @@ public class SuperArray {
     public int get(int index) { return mData[index]; }
 	
     public int size(){ return mSize; }
+	//public int capacity(){ return mData.length; }
     
     public static void main(String[] args) {
 		SuperArray s = new SuperArray();
+		SuperArray t = new SuperArray();
+		SuperArray q = new SuperArray();
+		int f;
+		
 		for (int x = 0; x < 10; x++) {
 			s.add(x);
+			q.add(x);
 		}
+		//Standard created new array
 		System.out.println(s.toStringDebug());
+		System.out.println(t.toStringDebug());
+		System.out.println(q.toStringDebug());
+		
+		System.out.println("addition");
 		s.add(0, 1);
-		int f = s.set(1, 29);
-		f = s.remove(3);
 		System.out.println(s.toStringDebug());
-		System.out.println(f);
+		s.add(s.size() - 1, -67);	//Remember this shifts things
+		System.out.println(s.toStringDebug());
+		s.add(0);
+		System.out.println(s.toStringDebug());
+		
+		System.out.println("trimming");
+		s.trimToSize();
+		System.out.println("s: " + s.toStringDebug());
+		
+		t.trimToSize();
+		System.out.println("t: " + t.toStringDebug());
+		
+		q.trimToSize();
+		System.out.println("q: " + q.toStringDebug());
+		
+		System.out.println("removing");
+		f = s.remove(0);
+		System.out.println(s.toStringDebug()+" rem "+f);
+		f = s.remove(s.size() - 1);
+		System.out.println(s.toStringDebug()+" rem "+f);
+		s.remove(4);
+		System.out.println(s.toStringDebug());
+		
+		System.out.println("setting");
+		f = s.set(1, 29);
+		System.out.println(s.toStringDebug()+" replaced " + f);
+		f = s.set(0, 12345678);
+		System.out.println(s.toStringDebug()+" replaced "+f);
+		f = s.set(s.size() - 1, 90);
+		System.out.println(s.toStringDebug()+" replaced "+f);
+		
+		System.out.println("toArray");
+		s.add(2);
 		int[] test = s.toArray();
 		int[] test1 = new SuperArray().toArray();
-		System.out.println("testing toArray");
 		for (int x : test) System.out.print(x + " ");
 		System.out.println();
 		for (int x : test1) System.out.print(x + " ");
 		System.out.println();
+		
 		System.out.println("testing indexOf");
-		System.out.println(s.indexOf(29));		//1
-		System.out.println(s.indexOf(101));		//-1
-		System.out.println(s.indexOf(1));		//0
-		System.out.println(s.lastIndexOf(1));	//2
+		System.out.println(s.toStringDebug());
+		System.out.println(s.indexOf(2));		//2
+		System.out.println(s.indexOf(89));		//-1
+		System.out.println(s.indexOf(5));		//4
+		System.out.println(s.lastIndexOf(8));	//2
 		System.out.println(s.lastIndexOf(234));	//-1
+		System.out.println(s.lastIndexOf(2));	//7
+		
+		System.out.println("string debug");
+		t.add(7);
+		t.trimToSize();
+		System.out.println(t.toStringDebug());
+		System.out.println(t.size());
+		
+		System.out.println("clear");
+		t.clear();
+		t.add(4);
+		System.out.println(t.toStringDebug());
+		t.clear();
+		System.out.println(t.toStringDebug());
+		t.add(6);
+		t.add(4);
+		System.out.println(t.toStringDebug());
     }
 }

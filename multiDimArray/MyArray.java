dc654d51be4143e08a41c15517d8c78e363f1447
Stanley Lin <slin55055@gmail.com>
2016-11-10 12:28:22 -0500

    Wrote 2d array printing function

---
+++ multiDimArray/MyArray.java
@@ -0,0 +1,13 @@
+public class MyArray {
+    public static void print2d(int[][] a) {
+	System.out.print("[");
+	for (int[] x : a) {
+	    
+	}
+    }
+    
+    public static void main(String[] args) {
+	int[][] p = new int[2][3];
+    }
+
+}

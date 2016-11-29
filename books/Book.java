9139c17b0078dcb70fcee2ef4bc51dd8f72daa58
Stanley Lin <slin55055@gmail.com>
2016-11-29 03:41:51 -0500

    Started library lab

---
+++ books/Book.java
@@ -0,0 +1,21 @@
+public class Book {
+    private String mAuthor;
+    private String mTitle;
+    private String mIsbn;
+    
+    public Book() {}
+    public Book(String author, String title, String isbn) {
+	mAuthor = author;
+	mTitle = title;
+	mIsbn = isbn;
+    }
+    
+    public String getAuthor(){ return mAuthor;}
+    public void setAuthor(String author) { mAuthor = author;}
+    
+    public String getTitle() {return mTitle;}
+    public void setTitle(String title) {mTitle = title;}
+    
+    public String getIsbn() {return mIsbn;}
+    public void setIsbn(String isbn) {mIsbn = isbn;}
+}

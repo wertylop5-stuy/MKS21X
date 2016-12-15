0f8d77eea00bcce72138354ce216e1c75eef5062
Stanley Lin <slin55055@gmail.com>
2016-12-14 22:07:44 -0500

    finished insertionsort

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -7,68 +7,118 @@ public class Sorts {
      *@param the array to be sorted
      */
     public static void selectionSort(int[] data) {
-	int smallestIndex = 0;
-        for (int sortedIndex = 0; sortedIndex < data.length; sortedIndex++) {
-	    smallestIndex = sortedIndex;
-	    
-	    //Selects the smallest non-sorted number
-	    for (int counter = sortedIndex; counter < data.length; counter++) {
-		if (data[counter] < data[smallestIndex]) {
-		    smallestIndex = counter;
+		int smallestIndex = 0;
+		for (int sortedIndex = 0; sortedIndex < data.length; sortedIndex++) {
+			smallestIndex = sortedIndex;
+			
+			//Selects the smallest non-sorted number
+			for (int counter = sortedIndex; counter < data.length; counter++) {
+				if (data[counter] < data[smallestIndex]) {
+					smallestIndex = counter;
+				}
+			}
+			
+			//Swap integers
+			swap(data, sortedIndex, smallestIndex);
 		}
-	    }
-	    
-	    //Swap integers
-	    swap(data, sortedIndex, smallestIndex);
-	}
     }
     
     
     public static void insertionSort(int[] data) {
-	int temp = 0;
-	int j = 0;
-	
-	//Go thru all elements
-	for (int i = 1; i < data.length; i++) {
-	    System.out.println("current: " + data[i] + " " + i);
-	    if (data[i] < data[i-1]) {
-		System.out.println("from " + data[i] + " to " + i);
-		temp = data[i];
-		j = i;
-		while (j > 0 && data[j] < data[j-1]) {
-		    data[j] = data[j-1];
-		    j--;
+		int temp = 0;
+		int j = 0;
+		
+		//Go thru all elems
+		//Is the current less than the previous?
+			//save current val
+			//is current less than previous
+				//change index
+			//set current index to temp
+		
+		
+		//Go thru all elements
+		for (int i = 1; i < data.length; i++) {
+			//System.out.println("current: " + data[i] + " " + i);
+			
+			//Is the current less than the previous?
+			if (data[i] < data[i-1]) {
+				//System.out.println("compare " + data[i] + " to " + data[i-1]);
+				
+				//save current val
+				temp = data[i];
+				j = i;
+				
+				//is current less than previous
+				while (j > 0 && temp < data[j-1]) {
+					//System.out.println("moving index " + (j-1) + " to " + j);
+					data[j] = data[j-1];
+					//change index
+					j--;
+				}
+				//System.out.println(j > 0 && data[j] < data[j-1]);
+				data[j] = temp;
+			}
 		}
-		data[j] = temp;
-	    }
-	}
     }
     
     private static void swap(int[] data, int i, int j) {
-	int temp = data[i];
-	data[i] = data[j];
-	data[j] = temp;
+		int temp = data[i];
+		data[i] = data[j];
+		data[j] = temp;
     }
 
     private static void resetArrays(int[] master, int[] a, int[] b) {
-	System.arraycopy(master, 0, a, 0, a.length);
-	System.arraycopy(master, 0, b, 0, b.length);
+		System.arraycopy(master, 0, a, 0, a.length);
+		System.arraycopy(master, 0, b, 0, b.length);
+    }
+	private static void resetArrays(int[] master, int[] a/*, int[] b*/) {
+		System.arraycopy(master, 0, a, 0, a.length);
+		//System.arraycopy(master, 0, b, 0, b.length);
     }
     
     public static void main(String[] args) {
-	int[] a = {1, 0, -1, 45, 21, 6};
-	int[] b = {0, 1, 1, 2, 3, 4};
-	int[] ans = {1, 2, 3, 4, 5, 6};
-	resetArrays(a, b, ans);
-	
-	Arrays.sort(ans);
-	selectionSort(b);
-	System.out.println(Arrays.equals(b, ans));
-	
-	resetArrays(a, b, ans);
-	Arrays.sort(ans);
-	insertionSort(b);
-	System.out.println(Arrays.equals(b, ans));
-	for (int x:b) System.out.println(x);
+		int[] a = {1, 0, -1, 45, 21, 6};
+		int[] c = {0, 0, 1, 0, 0, 0};
+		int[] e = {1, 2, 3, 4, 5, 6};
+		int[] g = {6, 5, 4, 3, 2, 1};
+		
+		int[] z = {1};
+		
+		int[] b = {0, 1, 1, 2, 3, 4};
+		int[] d = {9};
+		int[] ans = {1, 2, 3, 4, 5, 6};
+		int[] as = {8};
+		
+		resetArrays(a, b, ans);
+		Arrays.sort(ans);
+		selectionSort(b);
+		System.out.println(Arrays.equals(b, ans));
+		
+		resetArrays(c, b, ans);
+		Arrays.sort(ans);
+		selectionSort(b);
+		System.out.println(Arrays.equals(b, ans));
+		
+		resetArrays(a, b, ans);
+		Arrays.sort(ans);
+		insertionSort(b);
+		System.out.println(Arrays.equals(b, ans));
+		
+		resetArrays(e, b, ans);
+		Arrays.sort(ans);
+		insertionSort(b);
+		System.out.println(Arrays.equals(b, ans));
+		
+		resetArrays(g, b, ans);
+		Arrays.sort(ans);
+		insertionSort(b);
+		System.out.println(Arrays.equals(b, ans));
+		
+		resetArrays(z, d, as);
+		Arrays.sort(as);
+		insertionSort(d);
+		selectionSort(d);
+		System.out.println(Arrays.equals(d, as));
+		
     }
 }
